2338f54c25d64bc2d5c466a8bf75812e3d472ce3
Bohdan Nalyvkin <nalivkin.bog@gmail.com>
2025-01-08 15:51:29 +0200

    !

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import { BrowserRouter as Router, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./App.css";
 import AppRoutes from "./utils/routes/route";
 import Header from "./components/header/header";
 
 const AppContent = () => {
   const location = useLocation();
+  const [isCssLoaded, setIsCssLoaded] = useState(false);
+
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "App.css";
+
+    link.onload = () => {
+      setIsCssLoaded(true);
+    };
+
+    document.head.appendChild(link);
+
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, []);
 
   const hideHeader = location.pathname.startsWith("/admin");
+
+  if (!isCssLoaded) {
+    return (
+      <div className="container">
+        <div className="loader"></div>
+      </div>
+    );
+  }
 
   return (
     <div className="container">
